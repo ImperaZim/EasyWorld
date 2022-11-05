f2f13ad92bc1d695a861d0094a201fccb5eb68fa
ImperaZim <92953608+ImperaZim@users.noreply.github.com>
2022-11-05 19:14:04 -0300

    Create WorldCombatEvent.php

---
+++ src/ImperaZim/EasyWorld/Events/WorldCombatEvent.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace ImperaZim\EasyWorld\Events;
+
+use pocketmine\utils\Config;
+use pocketmine\player\Player;
+use pocketmine\event\Listener;
+use ImperaZim\EasyWorld\Loader;
+use pocketmine\event\entity\EntityDamageEvent;
+use pocketmine\event\entity\EntityDamageByEntityEvent;
+
+class WorldCombatEvent implements Listener {
+
+ public function DamageEvent(EntityDamageEvent $event) {
+  $plugin = $this->getPlugin();
+  $player = $event->getEntity();
+  $message = $plugin->getConfig();
+  $world = $player->getWorld()->getDisplayName(); 
+  $config = new Config($plugin->getDataFolder() . "worlds.yml");
+  if($event->getCause() == 11){
+   $event->cancel();
+   $player->teleport($player->getWorld()->getSafeSpawn()); 
+   return true;
+  }
+  if (!$config->getAll()[$world]["combat"]) {
+   $event->cancel();
+   return true;
+  }
+ }
+ 
+ public function ByEntityEvent(EntityDamageByEntityEvent $event) {
+  $plugin = $this->getPlugin();
+  $damaged = $event->getEntity();
+  $message = $plugin->getConfig();
+   if (!$damaged instanceof Player) {
+   $damager = $event->getDamager();
+   $world = $player->getWorld()->getDisplayName(); 
+   $config = new Config($plugin->getDataFolder() . "worlds.yml");
+   if (!$config->getAll()[$world]["combat"]) {
+    $event->cancel();
+    $damager->sendMessage($plugin->ProcessTags(["{prefix}", "{world}"], [$message->get("plugin.prefix"), $world], $message->get("world.combat.off.message")));
+    $damaged->sendMessage($plugin->ProcessTags(["{prefix}", "{world}"], [$message->get("plugin.prefix"), $world], $message->get("world.combat.off.message")));
+    return true;
+   } 
+  }
+ }
+
+ public function getPlugin() {
+  return Loader::getInstance();
+ }
+
+}
