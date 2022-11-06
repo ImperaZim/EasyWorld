b34f6e393b6a37d9a5892734b81629db8731ad17
ImperaZim <92953608+ImperaZim@users.noreply.github.com>
2022-11-05 21:35:50 -0300

    Update BirchTree.php

--- src/ImperaZim/EasyWorld/Utils/generator/object/tree/BirchTree.php
+++ src/ImperaZim/EasyWorld/Utils/generator/object/tree/BirchTree.php
@@ -10,12 +10,6 @@ use pocketmine\world\BlockTransaction;
 
 class BirchTree extends GenericTree{
 
-	/**
-	 * Initializes this tree with a random height, preparing it to attempt to generate.
-	 *
-	 * @param Random $random the PRNG
-	 * @param BlockTransaction $transaction the BlockTransaction used to check for space and to fill wood and leaf
-	 */
 	public function __construct(Random $random, BlockTransaction $transaction){
 		parent::__construct($random, $transaction);
 		$this->setHeight($random->nextBoundedInt(3) + 5);
