73623d6e7e20d73390562f97f8400948f2abebe2
ImperaZim <92953608+ImperaZim@users.noreply.github.com>
2022-11-05 21:03:26 -0300

    Update TerrainObject.php

--- src/ImperaZim/EasyWorld/Utils/generator/object/TerrainObject.php
+++ src/ImperaZim/EasyWorld/Utils/generator/object/TerrainObject.php
@@ -13,7 +13,6 @@ use pocketmine\world\World;
 
 abstract class TerrainObject{
 
-	/** @var int[] */
 	private static array $PLANT_TYPES;
 
 	public static function init() : void{
@@ -23,16 +22,6 @@ abstract class TerrainObject{
 		}
 	}
 
-	/**
-	 * Removes weak blocks like grass, shrub, flower or mushroom directly above the given block, if present.
-	 * Does not drop an item.
-	 *
-	 * @param ChunkManager $world
-	 * @param int $x
-	 * @param int $y
-	 * @param int $z
-	 * @return bool whether a block was removed; false if none was present
-	 */
 	public static function killWeakBlocksAbove(ChunkManager $world, int $x, int $y, int $z) : bool{
 		$cur_y = $y + 1;
 		$changed = false;
@@ -50,16 +39,6 @@ abstract class TerrainObject{
 		return $changed;
 	}
 
-	/**
-	 * Generates this feature.
-	 *
-	 * @param ChunkManager $world the world to generate in
-	 * @param Random $random the PRNG that will choose the size and a few details of the shape
-	 * @param int $source_x the base X coordinate
-	 * @param int $source_y the base Y coordinate
-	 * @param int $source_z the base Z coordinate
-	 * @return bool if successfully generated
-	 */
 	abstract public function generate(ChunkManager $world, Random $random, int $source_x, int $source_y, int $source_z) : bool;
 }
 
