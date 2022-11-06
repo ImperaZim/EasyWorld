4864c267932ad0afea313048c5a90757de00d64c
ImperaZim <92953608+ImperaZim@users.noreply.github.com>
2022-11-05 22:18:16 -0300

    Update ClimateWhittakerMap.php

--- src/ImperaZim/EasyWorld/Utils/generator/biomegrid/ClimateWhittakerMap.php
+++ src/ImperaZim/EasyWorld/Utils/generator/biomegrid/ClimateWhittakerMap.php
@@ -10,16 +10,10 @@ class ClimateWhittakerMap {
 
 	public int $value;
 
-	/** @var int[] */
 	public array $cross_types;
 
 	public int $final_value;
 
-	/**
-	 * @param int $value
-	 * @param int[] $cross_types
-	 * @param int $final_value
-	 */
 	public function __construct(int $value, array $cross_types, int $final_value){
 	 $this->value = $value;
 	 $this->cross_types = $cross_types;
