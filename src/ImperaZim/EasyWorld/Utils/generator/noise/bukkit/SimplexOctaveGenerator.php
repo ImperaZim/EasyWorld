11b7d4920d8d7eb43b3977f6a8fa42607d32d14e
ImperaZim <92953608+ImperaZim@users.noreply.github.com>
2022-11-05 21:47:28 -0300

    Update SimplexOctaveGenerator.php

--- src/ImperaZim/EasyWorld/Utils/generator/noise/bukkit/SimplexOctaveGenerator.php
+++ src/ImperaZim/EasyWorld/Utils/generator/noise/bukkit/SimplexOctaveGenerator.php
@@ -8,11 +8,6 @@ use pocketmine\utils\Random;
 
 class SimplexOctaveGenerator extends BaseOctaveGenerator{
 
-	/**
-	 * @param Random $rand
-	 * @param int $octaves
-	 * @return SimplexNoiseGenerator[]
-	 */
 	private static function createOctaves(Random $rand, int $octaves) : array{
 		$result = [];
 
@@ -25,12 +20,6 @@ class SimplexOctaveGenerator extends BaseOctaveGenerator{
 
 	private float $w_scale = 1.0;
 
-	/**
-	 * Creates a simplex octave generator for the given {@link Random}
-	 *
-	 * @param Random $rand
-	 * @param int $octaves Amount of octaves to create
-	 */
 	public function __construct(Random $rand, int $octaves){
 		parent::__construct(self::createOctaves($rand, $octaves));
 	}
@@ -40,36 +29,14 @@ class SimplexOctaveGenerator extends BaseOctaveGenerator{
 		$this->setWScale($scale);
 	}
 
-	/**
-	 * Gets the scale used for each W-coordinates passed
-	 *
-	 * @return float W scale
-	 */
 	public function getWScale() : float{
 		return $this->w_scale;
 	}
 
-	/**
-	 * Sets the scale used for each W-coordinates passed
-	 *
-	 * @param float $scale New W scale
-	 */
 	public function setWScale(float $scale) : void{
 		$this->w_scale = $scale;
 	}
 
-	/**
-	 * Generates noise for the 3D coordinates using the specified number of
-	 * octaves and parameters
-	 *
-	 * @param float $x X-coordinate
-	 * @param float $y Y-coordinate
-	 * @param float $z Z-coordinate
-	 * @param float $frequency How much to alter the frequency by each octave
-	 * @param float $amplitude How much to alter the amplitude by each octave
-	 * @param bool $normalized If true, normalize the value to [-1, 1]
-	 * @return float resulting noise
-	 */
 	public function octaveNoise(float $x, float $y, float $z, float $frequency, float $amplitude, bool $normalized) : float{
 		$result = 0.0;
 		$amp = 1.0;
@@ -94,19 +61,6 @@ class SimplexOctaveGenerator extends BaseOctaveGenerator{
 		return $result;
 	}
 
-	/**
-	 * Generates noise for the 3D coordinates using the specified number of
-	 * octaves and parameters
-	 *
-	 * @param float $x X-coordinate
-	 * @param float $y Y-coordinate
-	 * @param float $z Z-coordinate
-	 * @param float $w W-coordinate
-	 * @param float $frequency How much to alter the frequency by each octave
-	 * @param float $amplitude How much to alter the amplitude by each octave
-	 * @param bool $normalized If true, normalize the value to [-1, 1]
-	 * @return float resulting noise
-	 */
 	public function noise(float $x, float $y, float $z, float $w, float $frequency, float $amplitude, bool $normalized = false) : float{
 		$result = 0.0;
 		$amp = 1.0;
@@ -118,7 +72,6 @@ class SimplexOctaveGenerator extends BaseOctaveGenerator{
 		$z *= $this->z_scale;
 		$w *= $this->w_scale;
 
-		/** @var SimplexNoiseGenerator $octave */
 		foreach($this->octaves as $octave){
 			$result += $octave->noise($x * $freq, $y * $freq, $z * $freq, $w * $freq) * $amp;
 			$max += $amp;
