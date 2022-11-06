9e908d509adabef30a20d0d5fa5e90c6cbdde619
ImperaZim <92953608+ImperaZim@users.noreply.github.com>
2022-11-05 21:23:37 -0300

    Update JungleTree.php

--- src/ImperaZim/EasyWorld/Utils/generator/object/tree/JungleTree.php
+++ src/ImperaZim/EasyWorld/Utils/generator/object/tree/JungleTree.php
@@ -10,11 +10,6 @@ use pocketmine\world\BlockTransaction;
 
 class JungleTree extends GenericTree{
 
-	/**
-	 * Initializes this tree with a random height, preparing it to attempt to generate.
-	 * @param Random $random
-	 * @param BlockTransaction $transaction
-	 */
 	public function __construct(Random $random, BlockTransaction $transaction){
 		parent::__construct($random, $transaction);
 		$this->setHeight($random->nextBoundedInt(7) + 4);
