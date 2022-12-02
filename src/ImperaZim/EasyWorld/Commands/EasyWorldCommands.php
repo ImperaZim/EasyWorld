8099d9cb2a78af9e52902962204d39655f196fa7
ImperaZim <92953608+ImperaZim@users.noreply.github.com>
2022-12-02 13:39:31 -0300

    Update EasyWorldCommands.php

--- src/ImperaZim/EasyWorld/Commands/EasyWorldCommands.php
+++ src/ImperaZim/EasyWorld/Commands/EasyWorldCommands.php
@@ -41,8 +41,6 @@ class EasyWorldCommands extends Command implements PluginOwned {
     if ((!isset($args[1])) || $args[1] == "1") {
      $player->sendMessage($plugin->ProcessTags(["{prefix}"],  [$message->get("plugin.prefix")], "{prefix} §7EasyWorld commands: [2/2] \n 
      §b-> §7/easyworld help
-     §b-> §7/easyworld create
-     §b-> §7/easyworld delete
      §b-> §7/easyworld teleport
      "));
      return true; 
@@ -51,7 +49,7 @@ class EasyWorldCommands extends Command implements PluginOwned {
      $player->sendMessage($plugin->ProcessTags(["{prefix}"],  [$message->get("plugin.prefix")], 
       "{prefix} §7EasyWorld commands: [2/2] \n 
       §b-> §7/easyworld list
-      §b-> §7/easyworld manager
+      §b-> §7/easyworld menu
       §b-> §7/easyworld command
       "));
      return true;
@@ -60,36 +58,6 @@ class EasyWorldCommands extends Command implements PluginOwned {
      $player->sendMessage($plugin->ProcessTags(["{prefix}"],  [$message->get("plugin.prefix")], "{prefix} §7usage: §b/easyworld help [1 or 2]")); 
     }
     $player->sendMessage($plugin->ProcessTags(["{prefix}"],  [$message->get("plugin.prefix")], "{prefix} §7usage: §b/easyworld help [1 or 2]")); 
-    return true;
-   }
-   if (in_array(strtolower($args[0]), ["create", "criar"])) {
-    if (isset($args[1])) {
-     $name = $args[1];
-     $seed = $args[2] ?? 1000;
-     $generator = $args[3] ?? "normal";
-     WorldCreate::execute($player, $name, $seed, $generator);
-     return true;
-    }
-    $player->sendMessage($plugin->ProcessTags(["{prefix}"], [$message->get("plugin.prefix")], $message->get("world.create.help")));
-    return true;
-   }
-   if (in_array(strtolower($args[0]), ["delete", "deletar"])) {
-    if (isset($args[1])) {
-     $name = $args[1];
-     WorldDelete::execute($player, $args[1]);
-     return true;
-    }
-    $player->sendMessage($plugin->ProcessTags(["{prefix}"], [$message->get("plugin.prefix")], $message->get("world.delete.help")));
-    return true;
-   }
-   if (in_array(strtolower($args[0]), ["rename", "renomear"])) {
-    if (isset($args[2])) {
-     $old = $args[1];
-     $new = $args[2];
-     WorldRename::execute($player, $old, $new);
-     return true;
-    }
-    $player->sendMessage($plugin->ProcessTags(["{prefix}"], [$message->get("plugin.prefix")], $message->get("world.rename.help")));
     return true;
    }
    if (in_array(strtolower($args[0]), ["manager", "settings"])) {
